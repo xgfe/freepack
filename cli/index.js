b2b5395b6fb8a271cc526db0168397bd185d9c46
Clunt <clunt@foxmail.com>
2018-05-29 12:48:32 +0800

    feat(cli): add freepack cli

--- cli/index.js
+++ cli/index.js
@@ -1,10 +1,26 @@
 "use strict";
 
+const freepack = require('../');
+const configYargs = require('./util/config-yargs');
+const configParse = require('./util/config-parse');
+
 
 exports.commands = [
 ];
 
-exports.builder = () => {};
+exports.builder = configYargs;
 
 exports.handler = argv => {
+    const config = configParse(argv);
+    freepack(config, packer => {
+        const time = packer.time;
+        console.log('freepack:', `${time.total()}ms`);
+        console.log('      id:', packer.id);
+        console.log('     src:', packer.src);
+        console.log('  output:', packer.output);
+        console.log('  backup:', packer.backup);
+        console.log('timeused:', time.list().map(([type, millisecond]) => `${type}[${millisecond}ms]`).join(', '));
+        console.log(' release:', JSON.stringify(packer.option.release));
+        console.log('  option:', JSON.stringify(packer.option));
+    });
 };
